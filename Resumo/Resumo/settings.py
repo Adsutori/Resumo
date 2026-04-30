339cc597ec58c9abf0729267b2f6240e2968067f
Adsutori <adsutori@outlook.com>
2026-04-30 22:23:32 +0200

    add Resend settings

--- Resumo/Resumo/settings.py
+++ Resumo/Resumo/settings.py
@@ -153,9 +153,12 @@ EMAIL_BACKEND       = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST          = 'smtp.gmail.com'
 EMAIL_PORT          = 587
 EMAIL_USE_TLS       = True
-EMAIL_HOST_USER     = config('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
-DEFAULT_FROM_EMAIL  = f'Resumo <{config("EMAIL_HOST_USER")}>'
+# EMAIL_HOST_USER     = config('EMAIL_HOST_USER')
+# EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+# DEFAULT_FROM_EMAIL  = f'Resumo <{config("EMAIL_HOST_USER")}>'
+EMAIL_HOST_USER     = 'resend'
+EMAIL_HOST_PASSWORD = config('RESEND_API_KEY')
+DEFAULT_FROM_EMAIL  = 'Resumo <onboarding@resend.dev>'
 
 
 # ==============================================================
